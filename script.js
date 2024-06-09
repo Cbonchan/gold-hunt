57f3632bdb27a4fe66d5d8cb4d57cec0d86457bb
D. Pérez C. <dperezc@fi.uba.ar>
2024-06-09 19:14:53 -0300

    Completada la tabla, disparos, identificadores de cada parte Inicialización automática de la tabla al iniciar la página, HitMarkers de cada área de la tabla, y clase para identificar cuál es disparada

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+const BALAS_ANTES_DE_RECARGAR = 7;
 let balas_disparadas = 0;
+let balas_totales_disparadas = 0;
+let puntos_totales = 0;
+let balas_falladas = 0;
 
-function reproducir_disparo() {
+function reproducirDisparo() {
     let audio = document.getElementById('fireSound');
     let audio2 = document.getElementById('fireSoundLastBullet');
-    if (balas_disparadas < 7){
+    if (balas_disparadas < BALAS_ANTES_DE_RECARGAR){
         audio.play();
         balas_disparadas++;
     }else{
@@ -11,3 +15,61 @@ function reproducir_disparo() {
         balas_disparadas = 0;
     }
 }
+
+function getBalasTotalesDisparadas() {
+    return balas_totales_disparadas;
+}
+
+function getPuntosTotales() {
+    return puntos_totales;
+}
+
+function getBalasFalladas() {
+    return balas_falladas;
+}
+
+function disparoAciertaAObjetivo(/*Aqui debería recibir la sección de la tabla sobre la que hizo click*/) {
+    /* if( seccion de la tabla es vacío (osea justo ahi nunca hay monedas) ) return false;
+        if( seccion de la tabla es una moneda && esVisible(moneda) ) return true;
+        return false;
+    */
+   return false;
+}
+
+function animarZonaDisparada(fila, columna){
+    zonaParaAnimar = document.getElementById( ((7 *(fila-1))) + columna);
+    zonaParaAnimar.classList.add('zonaEsDisparada')
+    setTimeout(() => {
+        zonaParaAnimar.classList.remove('zonaEsDisparada');
+    }, 200);
+}
+
+function disparar( fila , columna) {
+    reproducirDisparo();
+    animarZonaDisparada(fila,columna);
+    if( juego_iniciado){
+        balas_totales_disparadas++;
+        if( disparoAciertaAObjetivo(fila,columna) ){
+            asignar_puntos(fila,columna);
+        }else{
+            balas_falladas++;
+        }
+    }
+}
+
+function crearTabla() {
+    let tabla = '<table id= gameZone>';
+    for (let i = 0; i < 7; i++) {
+        tabla += '<tr>';
+        for (let j = 0; j < 7; j++) {
+            tabla += `<td 
+            onclick="disparar(${i + 1}, ${j + 1})"
+            bordercolor = "black"
+            id = "${i * 7 + j + 1}" >${i * 7 + j + 1}</td>`;
+        }
+        tabla += '</tr>';
+    }     
+    tabla += '</table>';    
+    document.getElementById('game').innerHTML = tabla;
+}
+window.onload = crearTabla;
