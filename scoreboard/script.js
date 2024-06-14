82333a9b9cca2c1133342cf4e45789bfb149ab23
Cbonchan <basterrasebastian@gmail.com>
2024-06-14 12:19:18 -0300

    fetch a la api funciona, el endpoint es de prueba momentaneamente

--- scoreboard/script.js
+++ scoreboard/script.js
@@ -1,19 +1,11 @@
-const data = [
-    // Ejemplo de objeto con los datos de un jugador
-    //!!! Importante, no es necesario ordenarlos por Score, ya que se encarga el backend de hacerlo y los envia en orden
-    //estos datos deberia devolver el backend al hacerle fetch a la api
-
-    {"name": "Juan Perez", "score": 85, "datetime": "10/07 14:30" },
-    {"name": "Ana Lopez", "score": 92, "datetime": "15/07 12:45" },
-    {"name": "Carlos Diaz", "score": 78, "datetime": "20/07 16:15" },
-    {"name": "Lucia Gomez", "score": 88, "datetime": "25/07 11:00" },
-    {"name": "Miguel Torres", "score": 95, "datetime": "01/08 09:30" },
-    {"name": "Sofia Ramirez", "score": 82, "datetime": "05/08 14:55" },
-    {"name": "Pedro Fernandez", "score": 76, "datetime": "10/08 13:45" },
-    {"name": "Marta Sanchez", "score": 90, "datetime": "15/08 10:30" },
-    {"name": "Diego Gutierrez", "score": 84, "datetime": "20/08 15:20" },
-    { "name": "Elena Morales", "score": 89, "datetime": "25/08 11:40" }
-  ];
+//el fetch es con informacion de prueba, cuando configuremos base de datos, fetch a otro endpoint
+  fetch('http://localhost:5000/scoreboard_prueba')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      createTable(data);
+    })
+    .catch((error) => {console.error('Error:', error); });
 
   function createTable(data) {
     const scoreboardDiv = document.getElementById('scoreboard');
@@ -49,5 +41,4 @@ const data = [
     scoreboardDiv.appendChild(table);
   }
 
-  createTable(data);
   
